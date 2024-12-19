9a89bf4cabb6985371747de001d318c4276aeef1
M4tthewDE <matthias.kronberg@gmail.com>
2024-12-19 22:50:50 +0100

    feat: manually refresh compose files

--- src/gui/ui/compose.rs
+++ src/gui/ui/compose.rs
@@ -1,14 +1,36 @@
+use std::sync::mpsc::Sender;
+
+use anyhow::{Context, Result};
 use egui::{Color32, RichText, Ui};
 use tracing::error;
 
+use crate::config::Config;
+use crate::proto::ComposeFile;
 use crate::state::compose::{ComposeFileDiff, DiffResult};
+use crate::state::State;
+use crate::update::StateChangeMessage;
 use crate::{client, App};
 
 impl App {
     pub fn compose(&self, ui: &mut Ui) {
         puffin::profile_function!();
 
-        ui.heading(RichText::new("Compose").color(Color32::WHITE));
+        ui.horizontal(|ui| {
+            ui.heading(RichText::new("Compose").color(Color32::WHITE));
+            if ui.button("⟳").clicked() {
+                let config = self.config.clone();
+                let tx = self.tx.clone();
+                self.rt.spawn(async move {
+                    if let Err(err) = update_compose_diffs(config, tx).await {
+                        error!("Update compose diff error: {err:?}");
+                    }
+                });
+            }
+        });
+
+        if self.state.compose_file_diffs.is_empty() {
+            return;
+        }
 
         ui.group(|ui| {
             ui.vertical(|ui| {
@@ -51,4 +73,26 @@ impl App {
             }
         });
     }
+}
+
+async fn update_compose_diffs(config: Config, tx: Sender<StateChangeMessage>) -> Result<()> {
+    let mut files = Vec::new();
+
+    for dir_entry in config.docker_compose_path.read_dir()? {
+        let dir_entry = dir_entry?;
+        files.push(ComposeFile {
+            name: dir_entry
+                .file_name()
+                .to_str()
+                .context("invalid file name {p:?}")?
+                .to_string(),
+            content: std::fs::read_to_string(dir_entry.path())?,
+        });
+    }
+
+    let diffs = crate::client::compose::diff_files(files, config.server_address).await?;
+
+    Ok(tx.send(Box::new(move |state: &mut State| {
+        state.compose_file_diffs = diffs;
+    }))?)
 }
--- src/gui/update.rs
+++ src/gui/update.rs
@@ -1,30 +1,19 @@
-use anyhow::{Context, Result};
-use futures::future::BoxFuture;
+use anyhow::Result;
 use std::{
     sync::mpsc::Sender,
     time::{Duration, Instant},
 };
 use tracing::warn;
 
-use crate::{
-    client::{compose, info},
-    config::Config,
-    proto::ComposeFile,
-    state::State,
-};
+use crate::{client::info, config::Config, state::State};
 
 pub type StateChangeMessage = Box<dyn FnOnce(&mut State) + Send + Sync>;
 
 pub async fn update(tx: Sender<StateChangeMessage>, config: Config) -> Result<()> {
     let start = Instant::now();
-    let futures: Vec<BoxFuture<Result<StateChangeMessage>>> = vec![
-        Box::pin(update_info(config.server_address.clone())),
-        Box::pin(update_compose(config)),
-    ];
 
-    for result in futures::future::join_all(futures).await {
-        tx.send(result?)?;
-    }
+    let state_change_message = update_info(config.server_address.clone()).await?;
+    tx.send(state_change_message)?;
 
     let elapsed = Instant::now() - start;
     if elapsed > Duration::from_millis(500) {
@@ -39,27 +28,5 @@ async fn update_info(server_address: String) -> Result<StateChangeMessage> {
 
     Ok(Box::new(move |state: &mut State| {
         state.info = info;
-    }))
-}
-
-async fn update_compose(config: Config) -> Result<StateChangeMessage> {
-    let mut files = Vec::new();
-
-    for dir_entry in config.docker_compose_path.read_dir()? {
-        let dir_entry = dir_entry?;
-        files.push(ComposeFile {
-            name: dir_entry
-                .file_name()
-                .to_str()
-                .context("invalid file name {p:?}")?
-                .to_string(),
-            content: std::fs::read_to_string(dir_entry.path())?,
-        });
-    }
-
-    let diffs = compose::diff_files(files, config.server_address).await?;
-
-    Ok(Box::new(move |state: &mut State| {
-        state.compose_file_diffs = diffs;
     }))
 }
